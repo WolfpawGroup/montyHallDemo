8dc6efecf65f5587c5bd027ef3fde16763c925fb
WolfyDen <wolfyden@gmail.com>
2018-06-03 21:34:26 +0200

    cxv

--- montyHallDemo/Program.cs
+++ montyHallDemo/Program.cs
@@ -8,8 +8,62 @@ namespace montyHallDemo
 {
 	class Program
 	{
+		public static double WinPercent = 0d;
+		public static double SwitchPercent = 0d;
+		public static double StayPercent = 0d;
+
+		public static bool showProgress = false;
+
 		static void Main(string[] args)
 		{
+			if(args != null && args.Length > 0)
+			{
+				showProgress = true;
+			}
+
+			intro();
+			start();
+		}
+
+		public static void intro()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Greeting goes here");
+			Console.ForegroundColor = ConsoleColor.Gray;
+		}
+
+		public static void start()
+		{
+			door 
+				choiceA = door.lose, 
+				choiceB = door.lose, 
+				choiceC = door.lose;
+
+			List<door> lstdr = new List<door>();
+
+			int tmp = new Random().Next();
+
+			if (tmp % 3 == 0)
+			{
+				choiceA = door.win;
+			}
+			else if (tmp % 2 == 0)
+			{
+				choiceB = door.win;
+			}
+			else
+			{
+				choiceC = door.win;
+			}
+
+
+		}
+
+		public enum door
+		{
+			win,
+			lose,
+			none
 		}
 	}
 }
